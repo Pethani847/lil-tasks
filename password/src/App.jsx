0308a571098735b4bd2b9c7ad941affb6195eb67
Madhvi Pethani <madhvipethani856@gmail.com>
2026-07-07 11:29:19 +0530

    tailwind fix

--- password/src/App.jsx
+++ password/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <>
     <div className='flex flex-col justify-center items-center h-screen'>
     <h1 className='text-3xl mb-5'>Password Hide and Seek</h1>
-      <div className='border-2 rounded-3xl py-2 px-4 flex justify-between w-60 items-center px-2 '>
+      <div className='border-2 rounded-3xl py-2 px-4 flex justify-between w-60 items-center'>
       <input value={pass} onChange={(e) => setPass(e.target.value)} type={show ? 'text' : 'password'} name='password' className='focus:outline-none w-full' placeholder='password'  />
       <button onClick={showHandler}>{show ? <i className="fa-solid fa-eye"></i> : <i className="fa-solid fa-eye-slash"></i>}</button>
      </div>
